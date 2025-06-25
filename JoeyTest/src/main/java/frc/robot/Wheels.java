7645af088d0e9de85e698949c6b3ff31e5bfc1e6
Joey <joeymindyas@gmail.com>
2025-06-24 20:34:11 -0400

    WhateverMessageYouThinkIsGood

---
+++ JoeyTest/src/main/java/frc/robot/Wheels.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Wheels {
+    private int wheelPos;
+    public Wheels() {
+        wheelPos = 1;
+    } // Constructor-Define Variables here and run start up code
+
+    public void printWheelPos() {
+        System.out.println(wheelPos);
+    }
+}
